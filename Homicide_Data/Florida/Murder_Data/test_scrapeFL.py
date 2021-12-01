e1c0cfa8d2cf3260c18e2540ff1ad23e52d172ed
Kelly Quinn <homicide.alerts@gmail.com>
2021-12-01 13:53:54 -0500

    Working scrape download of Florida Data

--- Homicide_Data/Florida/Murder_Data/test_scrapeFL.py
+++ Homicide_Data/Florida/Murder_Data/test_scrapeFL.py
@@ -1,17 +1,19 @@
 #Florida
+#Murder Data
 
 #Import libraries
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time 
+from time import sleep
 import pandas as pd
 
 #Set Chrome Options
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
-prefs = {"download.default_directory":"/Users/kellyquinn/Desktop/ORISE/HSSP_Code/Project_01/Homicide_Data/Florida"}
+prefs = {"download.default_directory":"/Users/kellyquinn/Desktop/ORISE/HSSP_Code/Project_01/Homicide_Data/Florida/Murder_Data"}
 options.add_experimental_option('prefs', prefs)
 
 #Set Chrome Driver 
@@ -23,7 +25,15 @@ driver.implicitly_wait(10)
 #Fetch webpage data
 driver.get("https://transparency.jaxsheriff.org/HOTS/Murder")
 
-print("Hello")
+dwnload = driver.find_element(By.XPATH, "//*[@id='M_C_MainContent_MainContent_ecExport_hlExport']")
+dwnload.click()
 
+# Pause 10 seconds to allow CSV to download
+sleep(10)
+
+# Read the csv into the console
+data_csv = open("/Users/kellyquinn/Desktop/ORISE/HSSP_Code/Project_01/Homicide_Data/Florida/Murder_Data/Exported Data.csv", "r")
+print(data_csv.read())
+      
 #Close browser window when complete
 driver.close()
