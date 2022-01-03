dcb008e807429083a159af0abed10e4579b3ef3a
Kelly Quinn <homicide.alerts@gmail.com>
2022-01-03 10:55:16 -0500

    1/3/22 Update

--- Homicide_Data/LittleRock/test_scrapeLR.py
+++ Homicide_Data/LittleRock/test_scrapeLR.py
@@ -12,7 +12,6 @@ import pandas as pd
 #Set Chrome Options
 options = Options()
 options.add_argument('--headless')
-options.add_argument('--disable-gpu')
 prefs = {"download.default_directory":"/Users/kellyquinn/Desktop/ORISE/HSSP_Code/Project_01/Homicide_Data/LittleRock"}
 options.add_experimental_option('prefs', prefs)
 
@@ -27,7 +26,7 @@ driver.get("https://data.louisvilleky.gov/dataset/crime-reports")
 dwnload = driver.find_element(By.XPATH, "//*[@id='data-and-resources']/div/div/ul/li[1]/div/span/a")
 dwnload.click()
 
-sleep(120)
+sleep(180)
 
 #Close browser window when complete
 driver.close()
@@ -35,4 +34,3 @@ driver.close()
 #Read .csv file
 df = pd.read_csv('Homicide_Data/LittleRock/CRIME_DATA_2021.csv')
 print(df)
-
