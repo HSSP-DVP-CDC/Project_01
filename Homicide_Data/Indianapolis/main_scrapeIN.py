93220904cc20e536e2122a0c55465ae89401e542
Kelly Quinn <homicide.alerts@gmail.com>
2022-01-03 11:58:30 -0500

    Name changes to files

---
+++ Homicide_Data/Indianapolis/main_scrapeIN.py
@@ -0,0 +1,53 @@
+#INDIANAPOLIS
+#https://databases.indystar.com/indianapolis-crime-list-of-all-criminal-homicides-in-2021/
+#TUTORIAL: https://www.youtube.com/watch?v=JLDbAx6LAdo
+
+#Import libraries
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import pandas as pd
+
+#Set Chrome Options
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+
+#Set Chrome Driver 
+chrome_path = r"/Users/kellyquinn/Desktop/ORISE/HSSP_Code/chromedriver"
+driver = webdriver.Chrome(chrome_path, chrome_options=options)
+driver.implicitly_wait(10)
+
+#Fetch webpage data
+driver.get("https://databases.indystar.com/indianapolis-crime-list-of-all-criminal-homicides-in-2021/")
+
+incident_number = driver.find_elements(By.XPATH, "//*[@id='csp-data']/div/div[3]/div/div[1]/table/tbody/tr/td[1]/a")
+incident_date = driver.find_elements(By.XPATH, "//*[@id='csp-data']/div/div[3]/div/div[1]/table/tbody/tr/td[2]")
+incident_location = driver.find_elements(By.XPATH, "//*[@id='csp-data']/div/div[3]/div/div[1]/table/tbody/tr/td[3]")
+victim_id = driver.find_elements(By.XPATH, "//*[@id='csp-data']/div/div[3]/div/div[1]/table/tbody/tr/td[4]")
+victim_race = driver.find_elements(By.XPATH, "//*[@id='csp-data']/div/div[3]/div/div[1]/table/tbody/tr/td[5]")
+victim_sex = driver.find_elements(By.XPATH, "//*[@id='csp-data']/div/div[3]/div/div[1]/table/tbody/tr/td[6]")
+victim_age = driver.find_elements(By.XPATH, "//*[@id='csp-data']/div/div[3]/div/div[1]/table/tbody/tr/td[7]")  
+incident_method = driver.find_elements(By.XPATH, "//*[@id='csp-data']/div/div[3]/div/div[1]/table/tbody/tr/td[8]")                    
+
+#Homcide Results
+homicide_results =[]
+
+for i in range(len(incident_number)):
+    temporary_data = {"Incident Number" : incident_number[i].text,
+                      "Incident Date" : incident_date[i].text,
+                      "Incident Location" : incident_location[i].text,
+                      "Victim ID" : victim_id[i].text,
+                      "Victim Race" : victim_race[i].text,
+                      "Victim Sex" : victim_sex[i].text,
+                      "Victim Age" : victim_age[i].text,
+                      "Incident Method" : incident_method[i].text
+                      }
+    homicide_results.append(temporary_data)
+
+#Create a dataframe using Pandas
+df = pd.DataFrame(homicide_results)
+print(df)
+
+#Export data to excel
+df.to_excel('indianapolis_homicide_data-headless.xlsx', index = False)
---
+++ Homicide_Data/Louisville/main_scrapeKY.py
@@ -0,0 +1,44 @@
+#Louisville
+#Kentucky
+
+#Import libraries
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time 
+from time import sleep
+import pandas as pd
+
+#Set Chrome Options
+options = Options()
+options.add_argument('--headless')
+prefs = {"download.default_directory":"/Users/kellyquinn/Desktop/ORISE/HSSP_Code/Project_01/Homicide_Data/Louisville"}
+options.add_experimental_option('prefs', prefs)
+
+#Set Chrome Driver 
+chrome_path = r"/Users/kellyquinn/Desktop/ORISE/HSSP_Code/chromedriver"
+driver = webdriver.Chrome(chrome_path, chrome_options=options)
+driver.implicitly_wait(10)
+
+#Fetch webpage data
+driver.get("https://data.louisvilleky.gov/dataset/crime-reports")
+
+#Set Chrome Driver 
+chrome_path = r"/Users/kellyquinn/Desktop/ORISE/HSSP_Code/chromedriver"
+driver = webdriver.Chrome(chrome_path, chrome_options=options)
+driver.implicitly_wait(10)
+
+#Fetch webpage data
+driver.get("https://data.louisvilleky.gov/dataset/crime-reports")
+
+dwnload = driver.find_element(By.XPATH, "//*[@id='data-and-resources']/div/div/ul/li[1]/div/span/a")
+dwnload.click()
+
+sleep(180)
+
+#Close browser window when complete
+driver.close()
+
+#Read .csv file
+df = pd.read_csv('Homicide_Data/Louisville/CRIME_DATA_2021.csv')
+print(df)
